a9901aa3251b47b1be4487944b765dffeb21c706
KABIR <kabir_karamatjanov@mail.ru>
2023-01-27 22:26:07 +0500

    steps complete

--- test2/src/Components/Steps/Steps.jsx
+++ test2/src/Components/Steps/Steps.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import style from "./Steps.module.scss";
 
 function Steps() {
   return (
-    <main>
-      <div className={style.StepsContainer}></div>
+    <main
+      style={{
+        backgroundImage:
+          "url('https://images01.nicepage.com/c461c07a441a5d220e8feb1a/ffe5ab9d13ef52cca44a3aa7/cvvcv-min.jpg')",
+      }}
+    >
+      <article>
+        <h1>01</h1>
+        <p>
+          Sample text. Click to select the text box. Click again or double click
+          to start editing the text. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+      </article>
+
+      <article>
+        <h1>02</h1>
+        <p>
+          Sample text. Click to select the text box. Click again or double click
+          to start editing the text. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+      </article>
+
+      <article>
+        <h1>03</h1>
+        <p>
+          Sample text. Click to select the text box. Click again or double click
+          to start editing the text. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+      </article>
     </main>
   );
 }
